73e39e3bbe43dd8bf1101e70caabc25e2639431f
Artem Safin <artemsafin.webdev@gmail.com>
2022-11-07 14:37:08 +0300

    first commit

---
+++ src/js/tariffsSlider.js
@@ -0,0 +1,28 @@
+import { Swiper } from 'swiper';
+import { IS_MOBILE } from './utils';
+
+export default function tariffsSlider() {
+    if (IS_MOBILE) return;
+    const elements = Array.from(document.querySelectorAll('.js-tariffs-slider'));
+
+    function initializeSlider(element) {
+        const container = element.querySelector('.swiper');
+
+        new Swiper(container, {
+            slidesPerView: 'auto'
+        });
+    }
+
+    elements.forEach(element => {
+        initializeSlider(element);
+    });
+
+    function initializeACFSlider($block) {
+        console.log('Native slider element', $block[0]);
+        initializeSlider($block[0]);
+    }
+
+    if (window.acf) {
+        window.acf.addAction('render_block_preview/type=gallery-slider', initializeACFSlider);
+    }
+}
